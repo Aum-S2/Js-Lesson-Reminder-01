5b76cdc1cc0ff10a4f7a23bfb793d341fe24170d
Aum-S2 <sorasitdb7375@hotmail.com>
2019-03-21 15:46:25 +0700

    Js leasson Reminder 01

---
+++ main.js
@@ -0,0 +1,32 @@
+// console
+/* 
+console.log('hello world');
+console.error('This is an Error');
+console.warn('This is a Warning');
+*/
+
+
+// let (let can reassign value), const (it Cant be changed)
+/*
+let age = 24;
+age = 42;
+console.log(age);
+
+const items = 11;
+items = 21;
+console.log(items);
+*/
+
+// string, numbers, boolean, null, undefined 
+/*
+const name = 'Robert';
+const age = 33;
+const rating = 4.4;
+const isFat = true;
+const item = null;
+const gift = undefined;
+let z;
+
+console.log(typeof name);
+*/
+
