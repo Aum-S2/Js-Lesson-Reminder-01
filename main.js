ee49639efc49090778cdfa1a8f3167106d3fed5b
Aum-S2 <sorasitdb7375@hotmail.com>
2019-03-22 14:59:58 +0700

    array method

--- main.js
+++ main.js
@@ -41,4 +41,33 @@ console.log('my name is '+ name +' and i am '+ age);
 // Template String
 const Hello = `My name is ${name} and i am ${age}`;
 console.log(Hello);
+
+
+// String Property & Method
+const w = 'Rice, Food, Drink, Snack, Cake';
+// A Method Needs Parentheses / Property dont
+console.log(w.length);
+console.log(w.split(', '));
 */
+
+// Array - Variables that hold multiple values // Array is zero-based
+
+const Juices = new Array ('Orange Juice', 'Apple Juice', 'Watermelon Juice', 'Pineapple Juice');
+// if you see "new" keyword and then something after it is a constructor
+
+Juices[4] = 'Pears Juice'; // not recommended
+
+Juices.push('Mango Juice'); // use this one is a better way (added at the end)
+
+Juices.unshift('Blueberry Juice'); // This one will added in the first Array
+
+Juices.pop(); // This will remove the last Array
+
+console.log(Array.isArray(Juices)); // To check if this is an array (to get true/false value)
+
+console.log(Juices.indexOf('Applejuice')); // to get index of array (number of an array)
+
+console.log(Juices);
+
+
+
