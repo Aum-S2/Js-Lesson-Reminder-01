546c81ece0ec0846b56408a5a74df34eaa6e7ee0
Aum-S2 <sorasitdb7375@hotmail.com>
2019-05-07 15:16:49 +0700

    Submit/Manipulate The DOM in UI Not Back End

--- main.js
+++ main.js
@@ -360,11 +360,44 @@ const btn = document.querySelector('.btn');
 btn.style.background = 'red';
 */
 
+/*
+
 
 // Event
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', (e) => {
-  e.preventDefault();
-  document.querySelector('#my-form').style.background = '#eee';
+e.preventDefault();
+document.querySelector('#my-form').style.background = '#eee';
 });
+*/
+
+// Submit /Manipulate The DOM /Only In User InterFace Not Back END *****
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  if(nameInput.value === '' || emailInput.value === '') {
+    msg.classList.add('error');
+    msg.innerHTML = 'Please Enter All Fields';
+
+    setTimeout(() => msg.remove(), 2500);
+  } else{
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`
+    ${nameInput.value} : ${emailInput.value}`));
+
+    userList.appendChild(li);
+
+    // Clear Fields
+    nameInput.value = '';
+    emailInput.Input = '';
+  }
+}
